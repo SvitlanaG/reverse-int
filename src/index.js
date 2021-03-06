4f04579f3d2cb01eca023d93fca3770cf68b265a
Svitlana Grytsai <34807666+SvitlanaG@users.noreply.github.com>
2021-03-06 17:37:48 +0100

    feat: solve reverse-int task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
 module.exports = function reverse (n) {
-  
+    let reverseN = 0;
+        if (n < 0) {
+            n = n * (-1);
+            }
+        while (n % 10 === 0) {
+            n = n / 10;
+            }
+            reverseN = n.toString().split('').reverse().join('');
+    return reverseN;
 }
